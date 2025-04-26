81ad48ee780434199a368a6f8e20d9ff81949208
HuyHoangPb <huyhoang2875@gmail.com>
2025-04-26 20:07:14 +0700

    merge code

--- src/main/java/com/g15/library_system/view/managementView/NavigationPanel.java
+++ src/main/java/com/g15/library_system/view/managementView/NavigationPanel.java
@@ -25,67 +25,192 @@ public class NavigationPanel extends JPanel {
   private GridBagConstraints gbc;
 
   public NavigationPanel() {
-    setLayout(new FlowLayout());
-    setPreferredSize(new Dimension(250, 900));
-    setBackground(Color.WHITE);
-    //    gbc = new GridBagConstraints();
-    //    gbc.gridx = 1;
-    //    gbc.insets = new Insets(5, 15, 5, 15);
-    //    gbc.fill = GridBagConstraints.HORIZONTAL;
+    setLayout(new GridBagLayout());
+    //      setBackground(Style.MENU_BACKGROUND_COLOR);
+//    setBackground(Color.WHITE);
+    GridBagConstraints gbc = new GridBagConstraints();
 
-    //    ComponentTop componentTop = new ComponentTop();
-    //    gbc.gridy = 0;
-    //    gbc.weighty = 0.4;
-    //    add(componentTop, gbc);
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.insets = new Insets(10, 10, 15, 10);
+    gbc.anchor = GridBagConstraints.WEST;
+    gbc.fill = GridBagConstraints.NONE;
 
-    ButtonsPanel buttons = new ButtonsPanel();
-    //    gbc.gridy++;
-    //    gbc.weighty = 0.4;
-    //    gbc.fill = GridBagConstraints.BOTH;
-    add(buttons);
+    appLogo =
+            ButtonGenerator.createCustomButton(
+                    " FIT NLU",
+                    Style.FONT_BOLD_25,
+                    Style.PURPLE_MAIN_THEME,
+                    Color.WHITE,
+                    null,
+                    //              Color.GRAY,
+                    //              1,
+                    10,
+                    SwingConstants.CENTER,
+                    new Dimension(220, 60));
+    ButtonGenerator.setButtonIcon(
+            "src/main/java/view/LibraryUI/images/fit_nlu_logo.jpg", appLogo, 10);
+    appLogo.setIsDarkerWhenPress(false);
+    add(appLogo, gbc);
+    gbc.gridy++;
 
-    //    ComponentBottom componentBottom = new ComponentBottom();
-    //    gbc.gridy++;
-    //    gbc.weighty = 0.4;
-    //    gbc.anchor = GridBagConstraints.PAGE_END;
-    //    add(componentBottom, gbc);
+    JSeparator separatorBot = new JSeparator(SwingConstants.HORIZONTAL);
+    separatorBot.setPreferredSize(new Dimension(220, 1));
+    gbc.insets = new Insets(5, 10, 5, 10);
+    add(separatorBot, gbc);
+    gbc.gridy++;
+
+    dashBoardBt =
+            ButtonGenerator.createCustomButton(
+                    " Dashboard",
+                    Style.FONT_PLAIN_13,
+                    Color.WHITE,
+                    Style.PURPLE_MAIN_THEME,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon("src/main/java/Icon/dashboardIcon.png", dashBoardBt, 20);
+    add(dashBoardBt, gbc);
+    gbc.gridy++;
+
+    dashBoardBt =
+
+    manageBookBt =
+            ButtonGenerator.createCustomButton(
+                    " Manage Books",
+                    Style.FONT_PLAIN_13,
+                    Color.BLACK,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon(
+            "src/main/java/view/LibraryUI/icons/manageBook.png", manageBookBt, 18);
+    add(manageBookBt, gbc);
+    gbc.gridy++;
+
+    lendedBookBt =
+            ButtonGenerator.createCustomButton(
+                    " Lended Books",
+                    Style.FONT_PLAIN_13,
+                    Color.BLACK,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon(
+            "src/main/java/view/LibraryUI/icons/lendBookIcon.png", lendedBookBt, 15);
+    add(lendedBookBt, gbc);
+    gbc.gridy++;
+
+    returnBookBt =
+            ButtonGenerator.createCustomButton(
+                    " Return Books",
+                    Style.FONT_PLAIN_13,
+                    Color.BLACK,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon("src/main/java/Icon/customerIcon.png", returnBookBt, 20);
+    add(returnBookBt, gbc);
+    gbc.gridy++;
+
+    readerBt =
+            ButtonGenerator.createCustomButton(
+                    " Readers",
+                    Style.FONT_PLAIN_13,
+                    Color.BLACK,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon("src/main/java/Icon/orderIcon.png", readerBt, 20);
+    add(readerBt, gbc);
+    gbc.gridy++;
+
+    librarianBt =
+            ButtonGenerator.createCustomButton(
+                    " Librarians",
+                    Style.FONT_PLAIN_13,
+                    Color.BLACK,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon("src/main/java/Icon/inventoryIcon.png", librarianBt, 20);
+    add(librarianBt, gbc);
+    gbc.gridy++;
+
+    settingBt =
+            ButtonGenerator.createCustomButton(
+                    " Settings",
+                    Style.FONT_PLAIN_13,
+                    Color.BLACK,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon("src/main/java/Icon/user_15094854.png", settingBt, 20);
+    add(settingBt, gbc);
+    gbc.gridy++;
+
+    myAccountBt =
+            ButtonGenerator.createCustomButton(
+                    " My Account",
+                    Style.FONT_PLAIN_13,
+                    Color.BLACK,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon("src/main/java/Icon/iconNotification.png", myAccountBt, 20);
+    add(myAccountBt, gbc);
+    gbc.gridy++;
+
+    logoutBt =
+            ButtonGenerator.createCustomButton(
+                    " Logout",
+                    Style.FONT_PLAIN_13,
+                    Style.LOGOUT_RED,
+                    Color.WHITE,
+                    Style.HOVER_WHITE_DARKER,
+                    20,
+                    SwingConstants.LEFT,
+                    new Dimension(220, 45));
+    ButtonGenerator.setButtonIcon(
+            "src/main/java/view/LibraryUI/icons/logoutIcon.png", logoutBt, 25);
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int height = (int) screenSize.getHeight();
+    int buttonHeight = 50 + 75 + 50 * 8 + 5 + 50;
+    int logoutHeight = 40 + 45;
+    gbc.insets = new Insets(height - (buttonHeight + logoutHeight), 10, 5, 10);
+    add(logoutBt, gbc);
+    setOpaque(false);
+  }
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    Graphics2D g2d = (Graphics2D) g;
+    // Tạo màu gradient: từ màu trên (Color.BLUE) xuống màu dưới (Color.CYAN)
+    int width = getWidth();
+    int height = getHeight();
+
+    GradientPaint gp = new GradientPaint(0, 0,new  Color(23, 159, 219), 0, height, new Color(11, 24, 63));
+
+    g2d.setPaint(gp);
+    g2d.fillRect(0, 0, width, height);
   }
 
-  class ComponentTop extends JPanel {
-    public ComponentTop() {
-      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-      setBackground(new Color(227, 242, 253)); // good
-      setAlignmentX(Component.CENTER_ALIGNMENT);
-
-      //      avatar = new CircularImage(CurrentUser.URL, 100, 100, true);
-      //      avatar.setAlignmentX(Component.CENTER_ALIGNMENT);
-      //
-      //      name =
-      //          LabelConfig.createLabel(
-      //              "<html>" + CurrentUser.CURRENT_USER_V2.getFullName() + "<html>",
-      //              Style.FONT_BOLD_20,
-      //              Color.BLACK,
-      //              SwingConstants.CENTER);
-      //      name.setPreferredSize(new Dimension(230, 50));
-      //      name.setAlignmentX(Component.CENTER_ALIGNMENT);
-      //
-      //      role =
-      //          LabelConfig.createLabel("Manager", Style.FONT_PLAIN_15, Color.GRAY,
-      // SwingConstants.LEFT);
-      //      role.setPreferredSize(new Dimension(100, 100));
-      //      role.setAlignmentX(Component.CENTER_ALIGNMENT);
-      //
-      //      JSeparator separatorTop = new JSeparator(SwingConstants.HORIZONTAL);
-      //      separatorTop.setPreferredSize(new Dimension(320, 5));
-      //
-      //      add(avatar);
-      //      add(Box.createRigidArea(new Dimension(0, 10)));
-      //      add(name);
-      //      add(role);
-      //      add(Box.createRigidArea(new Dimension(0, 5)));
-      //      add(separatorTop);
-    }
-  }
 
   class ButtonsPanel extends JPanel {
     public ButtonsPanel() {
@@ -96,7 +221,7 @@ public class NavigationPanel extends JPanel {
 
       gbc.gridx = 0;
       gbc.gridy = 0;
-      gbc.insets = new Insets(10, 0, 15, 0);
+      gbc.insets = new Insets(10, 10, 15, 10);
       gbc.anchor = GridBagConstraints.WEST;
       gbc.fill = GridBagConstraints.NONE;
 
@@ -256,25 +381,7 @@ public class NavigationPanel extends JPanel {
       int logoutHeight = 40 + 45;
       gbc.insets = new Insets(height - (buttonHeight + logoutHeight), 0, 5, 0);
       add(logoutBt, gbc);
-    }
-  }
-
-  class ComponentBottom extends JPanel {
-    public ComponentBottom() {
-      setLayout(new BorderLayout());
-      setBackground(Style.MENU_BACKGROUND_COLOR);
-      JSeparator separatorBot = new JSeparator(SwingConstants.HORIZONTAL);
-      separatorBot.setPreferredSize(new Dimension(320, 1));
-      add(separatorBot, BorderLayout.NORTH);
-
-      JPanel wrapperPn = new JPanel(new GridLayout(0, 1, 0, 1));
-      wrapperPn.setBackground(Style.MENU_BACKGROUND_COLOR);
-      wrapperPn.setMaximumSize(new Dimension(200, 30));
-
-      add(wrapperPn, BorderLayout.CENTER);
-
-      credit = new JLabel(" Group 2");
-      add(credit, BorderLayout.SOUTH);
+      setOpaque(false);
     }
   }
 
