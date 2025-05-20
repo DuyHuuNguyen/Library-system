0ea1dac4f6d25437e941a02d29d1483bbfabaf2f
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-05-20 18:01:59 +0700

    hehe

--- src/main/java/com/g15/library_system/data/LibrarianData.java
+++ src/main/java/com/g15/library_system/data/LibrarianData.java
@@ -60,7 +60,7 @@ public class LibrarianData implements Data<Librarian>, LibrarianSubject {
                 .address("123 Main St")
                 .dateOfBirth(3487562934875L)
                 .avatarKey("avatar1")
-                .phoneNumber("123456789")
+                .phoneNumber("0123456789")
                 .build(),
             Librarian.builder()
                 .id(2L)
@@ -71,7 +71,7 @@ public class LibrarianData implements Data<Librarian>, LibrarianSubject {
                 .address("456 Elm St")
                 .dateOfBirth(3487562934876L)
                 .avatarKey("avatar2")
-                .phoneNumber("987654321")
+                .phoneNumber("0987654321")
                 .build(),
             Librarian.builder()
                 .id(2L)
@@ -82,7 +82,7 @@ public class LibrarianData implements Data<Librarian>, LibrarianSubject {
                 .address("456 Elm St")
                 .dateOfBirth(3487562934876L)
                 .avatarKey("avatar2")
-                .phoneNumber("987654321")
+                .phoneNumber("0987654321")
                 .build());
 
     this.librarians.addAll(Librarians);
