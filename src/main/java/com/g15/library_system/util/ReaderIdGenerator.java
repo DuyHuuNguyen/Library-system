95dc851eb808548e2945b56dd814383fd4918292
HuyHoangPb <huyhoang2875@gmail.com>
2025-05-26 22:53:29 +0700

    edit id generator

--- src/main/java/com/g15/library_system/util/ReaderIdGenerator.java
+++ src/main/java/com/g15/library_system/util/ReaderIdGenerator.java
@@ -5,9 +5,14 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class ReaderIdGenerator {
   private static final AtomicLong counter = new AtomicLong(1);
-  private static int year = LocalDate.now().getYear();
+  private static int currentYear = LocalDate.now().getYear();
 
-  public static Long generateId() {
+  public static synchronized Long generateId() {
+    int year = LocalDate.now().getYear();
+    if (year != currentYear) {
+      currentYear = year;
+      counter.set(1);
+    }
     String yearStr = String.valueOf(year).substring(2);
     String countStr = String.format("%04d", counter.getAndIncrement());
     String result = yearStr + countStr;
