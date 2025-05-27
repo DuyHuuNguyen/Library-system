6b78d45adf80da7de2f5f84169df3f983faec0b4
lengocchau <ngocchau9.007@gmail.com>
2025-05-27 12:34:57 +0700

    update

--- src/main/java/com/g15/library_system/entity/Librarian.java
+++ src/main/java/com/g15/library_system/entity/Librarian.java
@@ -1,10 +1,15 @@
 package com.g15.library_system.entity;
 
+import lombok.Getter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @ToString(callSuper = true)
 @SuperBuilder
+@Getter
 public class Librarian extends User {
   public boolean isSamePassword(String password) {
     return this.password.equals(password);
@@ -13,4 +18,20 @@ public class Librarian extends User {
   public void changePassword(String newPassword) {
     this.password = newPassword;
   }
+
+    public boolean hasSameName(String lastName) {
+        return this.lastName.equalsIgnoreCase(lastName);
+    }
+
+    public boolean isSameInfo(String text) {
+        return this.firstName.toLowerCase().contains(text.toLowerCase()) ||
+                this.lastName.toLowerCase().contains(text.toLowerCase()) ||
+                this.phoneNumber.contains(text);
+    }
+    public String getDateOfBirthAsString() {
+        if (dateOfBirth == null) return null;
+        Date date = new Date(dateOfBirth); // dateOfBirth được tính bằng milliseconds từ epoch
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); // định dạng ngày tháng
+        return sdf.format(date);
+    }
 }
